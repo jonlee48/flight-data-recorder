1eed68fc189a4405ccba1b9d0de6c623d57b42e2
jonlee48 <jonathan.lee48@gmail.com>
2021-01-16 20:48:46 -0500

    updated code for 3rd flight test

--- Arduino/fdr_record/fdr_record.ino
+++ Arduino/fdr_record/fdr_record.ino
@@ -1,56 +1,43 @@
+/*
+Outputs data in csv format with 27 values per line, 12 sensor readings + GPS NMEA GPGGA sentence
+Count, System Calibration level (0-3), Linear Acceleration XYZ (m/s^2), Gyro XYZ (radians/sec), Quaternion WXYZ, GPS NMEA GPGGA sentence
+*/
+
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 #include <SD.h> 
-#include <Adafruit_GPS.h>     // for GPS parsing
-
-/* This reads raw data from the BNO055
-
-Outputs data in csv format with 12 values per line:
-Count, System Calibration level (0-3), Linear Acceleration XYZ (m/s^2), Gyro XYZ (radians/sec), Quaternion WXYZ
-
-
-   Connections
-   ===========
-   Connect SCL to SCL
-   Connect SDA to SDA
-   Connect VDD to 3.3V DC
-   Connect GROUND to common ground
-
-   History
-   =======
-   1/4/2021
-*/
-
-// SET TO TRUE FOR SERIAL PRINT
-#define DEBUG false
-
+#include <Adafruit_GPS.h>
 
 /* Set the delay between fresh samples */
 #define SAMPLE_RATE (100)
 // SD:
 #define cardSelect 4      // M0 SD card
-
-
 // GPS:
 #define GPSSerial Serial1
+// Define some I/O buttons and LEDs
+#define BUTTON 10
+#define RECORD_LED 6 // I wired this wrong, uses inverted logic
+#define RECORD_SIGNAL A5
+#define STATUS_LED 8
+#define RECEIVE_LED 13
+#define RECEIVE_STATUS A2
 
 // Global Vars
-Adafruit_GPS GPS(&GPSSerial); // declare GPS
+Adafruit_GPS GPS(&GPSSerial);
 Adafruit_BNO055 bno = Adafruit_BNO055(-1, 0x28);
 unsigned long prev_time = 0;
 unsigned long count = -1;
-boolean initialized = false;
 File logfile;
 char filename[15];
 String GPSstr;
+uint8_t sys, gyro, accel, mag = 0;
 
-// for buttons
+// for button
 boolean record = false;
-unsigned long record_last;
+unsigned long record_last = 0;
 const int debounce = 1500;
-
 
 /**************************************************************************/
 /*
@@ -60,61 +47,40 @@ const int debounce = 1500;
 /**************************************************************************/
 void setup(void)
 {
-  pinMode(5, OUTPUT); // if initialized
-  pinMode(10, INPUT_PULLUP); // right button (record) on pin 10
-  pinMode(6, OUTPUT); // blue LED for record
-  pinMode(A4, OUTPUT); // signal for record
+  pinMode(STATUS_LED, OUTPUT); // green onboard LED lit if calibrated
+  pinMode(RECORD_LED, OUTPUT); // blue LED lit when recording
+  pinMode(RECORD_SIGNAL, OUTPUT); // signal to sensor to start/stop recording
+  pinMode(RECEIVE_STATUS, INPUT); // Red onboard LED lit when sensor is calibrated
+  pinMode(BUTTON, INPUT_PULLDOWN); // button to start/stop record
 
-  // GPS SETUP ---------------------------------------------------------------------------------------
+  digitalWrite(RECORD_LED, HIGH); // blue LED was wired wrong, has inverted logic
+
+  // GPS SETUP ************************************************************/
   GPSSerial.begin(9600);  // default rate for the Ultimate GPS
   // Tell GPS to output only GGA NMEA sentences
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_GGAONLY);
   // And send 10 updates per second
   GPS.sendCommand(PMTK_SET_NMEA_UPDATE_10HZ);
 
-  #ifdef DEBUG
   Serial.begin(115200);
   Serial.println("Orientation Sensor Raw Data Test"); Serial.println("");
-  #endif
   
   /* Initialise the sensor */
   if(!bno.begin())
   {
     /* There was a problem detecting the BNO055 ... check your connections */
-    #ifdef DEBUG
     Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
-    #endif
     while(1);
   }
 
-  // SD SETUP ----------------------------------------------------------------------------------------
+  // SD SETUP ************************************************************/
   // see if the card is present and can be initialized:
   if (!SD.begin(cardSelect)) {
-    #ifdef DEBUG
     Serial.println("Card init. failed!");
-    #endif
-  }
-  // create next FLIGHTXX.txt, don't open existing
-  strcpy(filename, "/FLIGHT00.TXT");
-  for (uint8_t i = 0; i < 100; i++) {
-    filename[7] = '0' + i/10;
-    filename[8] = '0' + i%10;
-    if (! SD.exists(filename)) {
-      break;
-    }
-  }
-  // open file
-  logfile = SD.open(filename, FILE_WRITE);
-  if(!logfile) {
-    #ifdef DEBUG
-    Serial.print("Couldnt create "); 
-    Serial.println(filename);
-    #endif
   }
   
   delay(1000);
   bno.setExtCrystalUse(true);
-
 }
 
 void getGPS() {
@@ -133,69 +99,61 @@ void getGPS() {
     should go here)
 */
 /**************************************************************************/
-void loop(void)
-{
-    uint8_t sys, gyro, accel, mag = 0;
 
-    if (!initialized && sys > 0) {
-    initialized = true;
-    digitalWrite(5, HIGH);
-  }
-
-  if (!initialized)
-    digitalWrite(5, LOW);
+void loop(void) {
+  if (digitalRead(RECEIVE_STATUS) == HIGH)
+    digitalWrite(RECEIVE_LED, HIGH);
+  else 
+    digitalWrite(RECEIVE_LED, LOW);
+  
+  getGPS(); // this needs to be called every loop
+  
+  /* Display calibration status for each sensor. */
+  bno.getCalibration(&sys, &gyro, &accel, &mag);
+  
+  // turn on green light if imu is calibrated
+  if (sys > 0)
+    digitalWrite(STATUS_LED, HIGH);
+  else
+    digitalWrite(STATUS_LED, LOW);
 
   
-    if (digitalRead(10) == LOW && millis() >= record_last + debounce) {
+  // if new record button is pressed
+  if (digitalRead(BUTTON) == HIGH && millis() >= record_last + debounce) {
     record_last = millis();
     record = !record;
     if (record) {
       Serial.println("start recording");
-      digitalWrite(6, HIGH);
-      digitalWrite(A4, HIGH);
+      digitalWrite(RECORD_LED, LOW);
+      digitalWrite(RECORD_SIGNAL, HIGH);
       count = -1;
-        // create next FLIGHTXX.txt, don't open existing
-  strcpy(filename, "/FLIGHT00.TXT");
-  for (uint8_t i = 0; i < 100; i++) {
-    filename[7] = '0' + i/10;
-    filename[8] = '0' + i%10;
-    if (! SD.exists(filename)) {
-      break;
-    }
-  }
-  // open file
-  logfile = SD.open(filename, FILE_WRITE);
-  if(!logfile) {
-    #ifdef DEBUG
-    Serial.print("Couldnt create "); 
-    Serial.println(filename);
-    #endif
-  }
+      // create new FLIGHTXX.txt
+      strcpy(filename, "/FLIGHT00.txt");
+      for (uint8_t i = 0; i < 100; i++) {
+        filename[7] = '0' + i/10;
+        filename[8] = '0' + i%10;
+        if (! SD.exists(filename)) {
+          break;
+        }
+      }
+      // open file
+      logfile = SD.open(filename, FILE_WRITE);
     }
     else {
       Serial.println("stop recording");
-      digitalWrite(6, LOW);
-      digitalWrite(A4, LOW);
+      digitalWrite(RECORD_LED, HIGH);
+      digitalWrite(RECORD_SIGNAL, LOW);
     }
   }
-  
-    getGPS(); // this needs to be called every loop
-    
-    /* Display calibration status for each sensor. */
-    bno.getCalibration(&sys, &gyro, &accel, &mag);
 
-
-
-
-  // only print if it's time to sample and the sensor is calibrated and record is pressed
-  if (millis() >= prev_time + SAMPLE_RATE && initialized && record) {
+  // only print if it's time to sample and record is pressed
+  if (millis() >= prev_time + SAMPLE_RATE && record) {
     prev_time = millis();
     
     imu::Vector<3> lin = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
     imu::Vector<3> rps = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
     imu::Quaternion quat = bno.getQuat();
 
-    #ifdef DEBUG
     // count (0++)
     Serial.print(++count); Serial.print(",");
     // calibration level (0-3)
@@ -214,7 +172,6 @@ void loop(void)
     Serial.print(quat.y(), 4); Serial.print(",");
     Serial.print(quat.z(), 4); Serial.print(",");
     Serial.print(GPSstr);
-    #endif
     
     logfile = SD.open(filename, FILE_WRITE);
     if (logfile) {
--- Arduino/sensor_record/sensor_record.ino
+++ Arduino/sensor_record/sensor_record.ino
@@ -1,46 +1,35 @@
+/* This reads raw data from the BNO055
+Outputs data in csv format with 12 values per line:
+Count, System Calibration level (0-3), Linear Acceleration XYZ (m/s^2), Gyro XYZ (radians/sec), Quaternion WXYZ
+*/
+
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 #include <SD.h> 
 
-/* This reads raw data from the BNO055
-
-Outputs data in csv format with 12 values per line:
-Count, System Calibration level (0-3), Linear Acceleration XYZ (m/s^2), Gyro XYZ (radians/sec), Quaternion WXYZ
-
-
-   Connections
-   ===========
-   Connect SCL to SCL
-   Connect SDA to SDA
-   Connect VDD to 3.3V DC
-   Connect GROUND to common ground
-
-   History
-   =======
-   1/4/2021
-*/
-
-// SET TO TRUE FOR SERIAL PRINT
-#define DEBUG false
-
-
 /* Set the delay between fresh samples */
 #define SAMPLE_RATE (100)
 // SD:
 #define cardSelect 4      // M0 SD card
+// Define some I/O
+#define RECORD_IN A1
+#define STATUS_OUT A0
+#define BUILTIN_LED 13
+
 
 // Global Vars
 Adafruit_BNO055 bno = Adafruit_BNO055(-1, 0x28);
 unsigned long prev_time = 0;
 unsigned long count = -1;
-boolean initialized = false;
-int leds[6] = {5,6,9,10,11,12};
+int leds[6] = {5,6,9,10,11,12}; // these correspond to the 6 LED pins
 File logfile;
 char filename[15];
-
 boolean record = false;
+boolean start_record = false;
+uint8_t sys, gyro, accel, mag = 0;
+
 
 /**************************************************************************/
 /*
@@ -50,51 +39,30 @@ boolean record = false;
 /**************************************************************************/
 void setup(void)
 {
-
-  pinMode(A1, INPUT);
+  pinMode(RECORD_IN, INPUT); // Audio jack input to start/stop record
+  pinMode(STATUS_OUT, OUTPUT); // Audio jack output if calibrated
+  pinMode(BUILTIN_LED, OUTPUT); // onboard LED
+  
   // set LED control pins
   for (int i=0; i<6; i++) {
     pinMode(leds[i], OUTPUT);
   }
 
-  #ifdef DEBUG
   Serial.begin(115200);
   Serial.println("Orientation Sensor Raw Data Test"); Serial.println("");
-  #endif
   
   /* Initialise the sensor */
   if(!bno.begin())
   {
     /* There was a problem detecting the BNO055 ... check your connections */
-    #ifdef DEBUG
     Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
-    #endif
     while(1);
   }
 
-  // SD SETUP ----------------------------------------------------------------------------------------
+  // SD SETUP *************************************************************/
   // see if the card is present and can be initialized:
   if (!SD.begin(cardSelect)) {
-    #ifdef DEBUG
     Serial.println("Card init. failed!");
-    #endif
-  }
-  // create next FLIGHTXX.txt, don't open existing
-  strcpy(filename, "/FLIGHT00.TXT");
-  for (uint8_t i = 0; i < 100; i++) {
-    filename[7] = '0' + i/10;
-    filename[8] = '0' + i%10;
-    if (! SD.exists(filename)) {
-      break;
-    }
-  }
-  // open file
-  logfile = SD.open(filename, FILE_WRITE);
-  if(!logfile) {
-    #ifdef DEBUG
-    Serial.print("Couldnt create "); 
-    Serial.println(filename);
-    #endif
   }
   
   delay(1000);
@@ -108,45 +76,62 @@ void setup(void)
     should go here)
 */
 /**************************************************************************/
-void loop(void)
-{
-  if (digitalRead(A1) == HIGH) {
+void loop(void) {
+  /* Display calibration status for each sensor. */
+  bno.getCalibration(&sys, &gyro, &accel, &mag);
+
+  if (digitalRead(RECORD_IN) == HIGH) {
+    if (record == false) 
+      start_record = true;
+    else
+      start_record = false;
     record = true;
   }
   else {
     record = false;
+    start_record = false;
   }
 
-    /* Display calibration status for each sensor. */
-    uint8_t sys, gyro, accel, mag = 0;
-    bno.getCalibration(&sys, &gyro, &accel, &mag);
+  // output signal through audio jack if calibrated
+  if (sys > 0) {
+    digitalWrite(STATUS_OUT, HIGH);
+    // turn on onboard led to signify it is on
+    digitalWrite(BUILTIN_LED, HIGH);
+  }
+  else {
+    digitalWrite(STATUS_OUT, LOW);
+    digitalWrite(BUILTIN_LED, LOW);
+  }
 
-  if (!initialized && sys > 0) {
-    initialized = true;
-    for (int i=0; i<6; i++) {
-      digitalWrite(leds[i], HIGH);
-      delay(50);
-      digitalWrite(leds[i], LOW);
-      delay(100);
-      digitalWrite(leds[i], HIGH);
-      delay(50);
-      digitalWrite(leds[i], LOW);
-      delay(100);
+  if (start_record) {
+    count = -1;
+    start_record = false;
+    // create next FLIGHTXX.txt, don't open existing
+    strcpy(filename, "/FLIGHT00.txt");
+    for (uint8_t i = 0; i < 100; i++) {
+      filename[7] = '0' + i/10;
+      filename[8] = '0' + i%10;
+      if (! SD.exists(filename)) {
+        break;
+      }
+    }
+    // open file
+    logfile = SD.open(filename, FILE_WRITE);
+    if(!logfile) {
+      Serial.print("Couldnt create "); 
+      Serial.println(filename);
     }
   }
 
-
-  // only print if it's time to sample and the sensor is calibrated AND received record signal
-  if (millis() >= prev_time + SAMPLE_RATE && initialized){// && record) {
+  // only print if it's time to sample and received record signal
+  if (millis() >= prev_time + SAMPLE_RATE && record) {
     prev_time = millis();
     
     imu::Vector<3> lin = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
     imu::Vector<3> rps = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
     imu::Quaternion quat = bno.getQuat();
 
-    #ifdef DEBUG
     // count (0++) 
-    /*
     Serial.print(++count); Serial.print(",");
     // calibration level (0-3)
     Serial.print(sys, DEC); Serial.print(",");
@@ -158,12 +143,10 @@ void loop(void)
     Serial.print(rps.x()); Serial.print(",");
     Serial.print(rps.y()); Serial.print(",");
     Serial.print(rps.z()); Serial.print(",");
-*/
     Serial.print(quat.w(), 4); Serial.print(",");
     Serial.print(quat.x(), 4); Serial.print(",");
     Serial.print(quat.y(), 4); Serial.print(",");
     Serial.println(quat.z(), 4);
-    #endif
     
     logfile = SD.open(filename, FILE_WRITE);
     if (logfile) {
