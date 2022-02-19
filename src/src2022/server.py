8f2556a6d6353e225da5af8d54044cd36851c669
jonlee48 <jonathan.lee48@gmail.com>
2022-02-19 15:28:44 -0500

    simpler server

--- src/src2022/server.py
+++ src/src2022/server.py
@@ -23,22 +23,21 @@ import queue
 import dash_daq as daq
 import os
 
-OFFLINE = True      # If False, read data from file. If True, read data from serial port
+OFFLINE = False      # If False, read data from file. If True, read data from serial port
 PORT = 'COM9'       # Port of Ground Station, check name in Arduino IDE > Tools > Port
 BAUD_RATE = 115200  # baud rate (e.g. 9600 or 115200)
 REFRESH_RATE = 1    # how fast graph updates (milliseconds)
 BATCH_RATE = 1000   # how long to wait to batch packets (milliseconds)
+X_POINTS = 600      # how many points on the graph x axis
 
 
 # Data format (of radio packet)
 format = [
-    'count',
     'millis',
     'airspeed',
     'altitude',
     'pitch',
     'roll',
-    'xaccel',
     'rssi',
 ]
 
@@ -96,34 +95,33 @@ fileThread = threading.Thread(target=fileReader, args=(queue,), name='fileReader
 app = dash.Dash(__name__)
 
 # define a line plot
-fig = make_subplots(specs=[[{'secondary_y': True}]])
-fig.add_trace(go.Scatter(x=df['count'], y=df['pitch'], mode='lines', name='pitch (deg)'), secondary_y=False)
-fig.add_trace(go.Scatter(x=df['count'], y=df['altitude'], mode='lines', name='altitude (m)'), secondary_y=False)
-fig.add_trace(go.Scatter(x=df['count'], y=df['airspeed'], mode='lines', name='airspeed (m/s)'), secondary_y=True)
+fig1 = px.line(df, x='millis', y='airspeed', title='airspeed (mph)')
+fig2 = px.line(df, x='millis', y='altitude', title='altitude (ft)')
+fig3 = px.line(df, x='millis', y='pitch', title='pitch (deg)')
+#fig1 = make_subplots(specs=[[{'secondary_y': True}]])
+#fig1.add_trace(go.Scatter(x=df['millis'], y=df['pitch'], mode='lines', name='pitch (deg)'), secondary_y=False)
+#fig1.add_trace(go.Scatter(x=df['millis'], y=df['altitude'], mode='lines', name='altitude (m)'), secondary_y=False)
+#fig1.add_trace(go.Scatter(x=df['millis'], y=df['airspeed'], mode='lines', name='airspeed (m/s)'), secondary_y=True)
 
 
 # layout the components of the web page
 app.layout = html.Div(children=[
-    html.H2(id='count', children=''),
     html.H2(id='millis', children=''),
     html.H2(id='airspeed', children=''),
     html.H2(id='altitude', children=''),
     html.H2(id='pitch', children=''),
     html.H2(id='roll', children=''),
-    html.H2(id='xaccel', children=''),
     html.H2(id='rssi', children=''),
     daq.Gauge(
         id='gauge-airspeed',
         showCurrentValue=True,
-        units='m/s',
+        units='ft',
         label='Airspeed',
         value=5,
         min=0,
         max=50
     ),
     daq.Gauge(
-        
-        
         id ='gauge-pitch',
         showCurrentValue=True,
         units = 'degrees',
@@ -136,47 +134,103 @@ app.layout = html.Div(children=[
     dcc.Input(id='file-input', type='text', value='Flight00.html', debounce=True),
     html.H3(id='save-status', children=''),
     html.Button('Save Plot', id='save', n_clicks=0),
-    # add our line plot to the page
-    dcc.Graph(
-        id='line-graph',
-        figure=fig
-    ),
-    dcc.Store(
-        id='batch-store',
-        data={},
-        storage_type='memory'
-    ),
-    dcc.Store(
-        id='render-queue',
-        data=dict,
-        storage_type='memory'
-    ),
+    # add line plots to the page
+    dcc.Graph(id='line1',figure=fig1),
+    dcc.Graph(id='line2',figure=fig2),
+    dcc.Graph(id='line3',figure=fig3),
     # triggers update to batch-store
     dcc.Interval(
         id='batch-interval',
         interval=BATCH_RATE,  # milliseconds
         n_intervals=0
     ),
-    # triggers update to line-graph
-    dcc.Interval(
-        id='render-interval',
-        interval=REFRESH_RATE,  # milliseconds
-        n_intervals=0
-    )
 ])
 
+
+
+# Callback function every interval
+# Returns updated figure
+@app.callback(
+              # graphs:
+              Output('line1', 'figure'),
+              Output('line2', 'figure'),
+              Output('line3', 'figure'),
+              # readouts:
+              Output('millis', 'children'),
+              Output('airspeed', 'children'),
+              Output('altitude', 'children'),
+              Output('pitch', 'children'),
+              Output('roll', 'children'),
+              Output('rssi', 'children'),
+              # gauges:
+              Output('gauge-airspeed','value'),
+              Output('gauge-pitch', 'value'),
+              # input trigger:
+              Input('batch-interval', 'n_intervals'),
+              # feed in figure state:
+              State('line1', 'figure'),
+              State('line2', 'figure'),
+              State('line3', 'figure'))
+def batchUpdate(n, fig1, fig2, fig3):
+    batchStart = len(df)
+    while not queue.empty():
+        line = queue.get()
+        data = line.split(',')
+        if len(data) == len(format):
+            data = [float(d) for d in data]
+            data[index['altitude']] = data[index['altitude']] * 3.2808 # m to ft
+            data[index['airspeed']] = data[index['airspeed']] * 2.23694 # m/s to mph
+            df.loc[len(df)] = data
+    batchEnd = len(df)
+
+    # update figure data with latest new data
+    latest = df[batchStart:batchEnd]
+    # update traces
+    if len(fig1['data']) == 0:
+        fig1['data'].append({'x': [], 'y': []})
+        fig2['data'].append({'x': [], 'y': []})
+        fig3['data'].append({'x': [], 'y': []})
+
+    fig1['data'][0]['x'].extend(latest.index.values.tolist())
+    fig1['data'][0]['y'].extend(latest['airspeed'])
+    fig2['data'][0]['x'].extend(latest.index.values.tolist())
+    fig2['data'][0]['y'].extend(latest['altitude'])
+    fig3['data'][0]['x'].extend(latest.index.values.tolist())
+    fig3['data'][0]['y'].extend(latest['pitch'])
+
+    # update readouts
+    millis = 'Millis: {}'.format(df['millis'].values[-1])
+    airspeed = 'Airspeed: {:.2f} mph'.format(float(df['airspeed'].values[-1]))
+    altitude = 'Altitude: {:.2f} ft'.format(float(df['altitude'].values[-1]))
+    pitch = 'Pitch: {:.2f} deg'.format(float(df['pitch'].values[-1]))
+    roll = 'Roll: {:.2f} deg'.format(float(df['roll'].values[-1]))
+    rssi = 'Rssi: {:.2f}'.format(float(df['rssi'].values[-1]))
+
+    # update gauges
+    gauge_val = float(df['airspeed'].values[-1])
+    gauge_pitch = float(df['pitch'].values[-1])
+
+
+    return fig1, fig2, fig3, millis, airspeed, altitude, pitch, roll, rssi, gauge_val, gauge_pitch
+
+
+# save line graphs
 @app.callback(
     Output('save-status', 'children'),
     Input('save', 'n_clicks'),
     State('file-input', 'value'),
-    State('line-graph', 'figure'),
+    State('line1', 'figure'),
+    State('line2', 'figure'),
+    State('line3', 'figure'),
 )
-def update_output(n_clicks, filename, fig):
+def update_output(n_clicks, filename, fig1, fig2, fig3):
     if n_clicks == 0:
         # in-place of prevent_initial_call=True
         return dash.no_update
 
-    figure = go.Figure(fig)
+    f1 = go.Figure(fig1)
+    f2 = go.Figure(fig2)
+    f3 = go.Figure(fig3)
 
     path = 'saves/'
     files = os.listdir(path)
@@ -184,119 +238,14 @@ def update_output(n_clicks, filename, fig):
     if filename in files:
         status = '{} already exists, enter a different name.'.format(filename)
     else:
-        figure.write_html(path+filename)
-        status = 'Saved plot as {}'.format(filename)
+        prefix, format = filename.split('.')
+        f1.write_html(path+prefix + '_airspeed' + '.' + format)
+        f2.write_html(path+filename + '_altitude' + '.' + format)
+        f3.write_html(path+filename + '_pitch' + '.' + format)
+
+        status = 'Saved plots as {}'.format(filename)
 
     return status
-
-
-# Callback function every interval
-# Returns updated figure
-@app.callback(Output('batch-store', 'data'),
-              Output('count', 'children'),
-              Output('millis', 'children'),
-              Output('airspeed', 'children'),
-              Output('altitude', 'children'),
-              Output('pitch', 'children'),
-              Output('roll', 'children'),
-              Output('xaccel', 'children'),
-              Output('rssi', 'children'),
-              Output('gauge-airspeed','value'),
-              Output('gauge-pitch', 'value'),
-              Input('batch-interval', 'n_intervals'))
-def batchUpdate(n):
-    batchStart = len(df)
-    while not queue.empty():
-        line = queue.get()
-        data = line.split(',')
-        #print(data)
-        #print(len(data))
-        df.loc[len(df)] = data
-
-    batchEnd = len(df)
-
-    # prep to send *all* columns
-    batch = df[batchStart:batchEnd]
-    dict = batch.to_dict('list')
-
-    #print('Batch updating {} items'.format(batchEnd-batchStart))
-
-    count = 'Count: {}'.format(df['count'].values[-1])
-    millis = 'Millis: {}'.format(df['millis'].values[-1])
-    airspeed = 'Airspeed: {:.2f} m/s'.format(float(df['airspeed'].values[-1]))
-    altitude = 'Altitude: {:.2f} m'.format(float(df['altitude'].values[-1]))
-    pitch = 'Pitch: {:.2f} deg'.format(float(df['pitch'].values[-1]))
-    roll = 'Roll: {:.2f} deg'.format(float(df['roll'].values[-1]))
-    xaccel = 'Xaccel: {:.2f} m/s^2'.format(float(df['xaccel'].values[-1]))
-    rssi = 'Rssi: {:.2f}'.format(float(df['rssi'].values[-1]))
-
-    gauge_val = float(df['airspeed'].values[-1])
-    gauge_pitch = float(df['pitch'].values[-1])
-    return dict, count, millis, airspeed, altitude, pitch, roll, xaccel, rssi, gauge_val, gauge_pitch
-
-
-# appends batch-store to render-queue whenever batch-store is updated
-app.clientside_callback(
-    # Javascript function executed in client browser
-    '''
-    function(batchStore, renderQueue) {
-        // append batchStore to renderQueue
-        keys = Object.keys(batchStore);
-        
-        for (let i = 0; i < batchStore[keys[0]].length; i++) {
-            if (keys[i] in renderQueue) {
-                renderQueue[keys[i]] = renderQueue[keys[i]].concat(batchStore[keys[i]]);
-            }
-            else {
-                renderQueue[keys[i]] = []; 
-            }
-        }
-
-        return renderQueue; 
-    } 
-    ''',
-    Output('render-queue', 'data'),
-    Input('batch-store', 'data'),
-    State('render-queue', 'data')
-)
-
-# takes first item in render_queue and appends it to the line-graph
-# triggered by render-interval
-app.clientside_callback(
-    # Javascript function executed in client browser
-    '''
-    function(n, renderQueue, figure) {
-        console.log(renderQueue['count'].length);
-        // deep copy figure
-        newFig = JSON.parse(JSON.stringify(figure));
-        //console.log(newFig);
-        // populate figure if first update
-        if (newFig['data'].length == 0) {
-            newFig['data'].push({'x': [], 'y': []})
-        }
-        // add datapoint to figure
-        // TODO: parameterize hardcoded count and pitch 
-        if (renderQueue['pitch'].length > 0) {
-            let x = Number(renderQueue['count'].shift());
-            let pitch = Number(renderQueue['pitch'].shift());
-            let altitude = Number(renderQueue['altitude'].shift());
-            let airspeed = Number(renderQueue['airspeed'].shift());
-            newFig['data'][0]['x'].push(x);
-            newFig['data'][0]['y'].push(pitch);
-            newFig['data'][1]['x'].push(x);
-            newFig['data'][1]['y'].push(altitude);
-            newFig['data'][2]['x'].push(x);
-            newFig['data'][2]['y'].push(airspeed);
-        }
-        //console.log(figure['data'][0]); 
-        return newFig; 
-    } 
-    ''',
-    Output('line-graph', 'figure'),
-    Input('render-interval', 'n_intervals'),
-    State('render-queue', 'data'),
-    State('line-graph', 'figure')
-)
 
 
 # run the web app
@@ -307,3 +256,4 @@ if __name__ == '__main__':
         readerThread.start()  # read from ground station
 
     app.run_server(port='7000', debug=True, use_reloader=False)
+
