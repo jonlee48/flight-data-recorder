122d3efe15847745e2ba29ea96d7a94566bb8166
jonlee48 <jonathan.lee48@gmail.com>
2022-02-19 15:25:05 -0500

    test code for two stepper motors

--- Arduino/dbf2022/tests/stepper_buttons/stepper_buttons.ino
+++ Arduino/dbf2022/tests/stepper_buttons/stepper_buttons.ino
@@ -1,27 +1,26 @@
 #include <Stepper.h>
 
-#define DEPLOY_PIN  4  // button to deploy box box n
-#define RESET_PIN   7  // button to reset n to NUM_BOXES
-#define FORWARD_PIN 5  // button to move belt forward by INTERVAL
-#define BACK_PIN    6  // button to move belt back by INTERVAL
+#define FORWARD_PIN 3  // button to move belt forward by INTERVAL
+#define BACK_PIN    5 // button to move belt back by INTERVAL
 
 #define STEPS 2048     // change this to fit the number of steps per revolution
 
 // initialize the stepper library on pins 8 through 11:
-Stepper myStepper(STEPS, 8, 10, 9, 11);
+Stepper myStepper1(STEPS, 8, 10, 9, 11);
+Stepper myStepper2(STEPS, 7, 4, 6, 2);
+
 
 void setup() {
   // set the speed at 10 rpm:
-  myStepper.setSpeed(10);
+  myStepper1.setSpeed(10);
+  myStepper2.setSpeed(10);
   // initialize the serial port:
   Serial.begin(9600);
 
   
   // buttons
-  pinMode(DEPLOY_PIN, INPUT_PULLUP);
-  pinMode(RESET_PIN, INPUT_PULLUP);
-  pinMode(FORWARD_PIN, INPUT_PULLUP);
   pinMode(BACK_PIN, INPUT_PULLUP);
+  pinMode(FORWARD_PIN, INPUT_PULLUP);
   
 }
 
@@ -33,16 +32,14 @@ void loop() {
   if (digitalRead(BACK_PIN) == LOW) {
     moveDegrees(-360);
   }
-
-  // step one revolution in the other direction:
-  //Serial.println("counterclockwise");
-  //myStepper.step(-stepsPerRevolution);
-  //delay(500);
 }
 
 // positive is clockwise
 void moveDegrees(int deg) {
   // convert degrees to steps
   int steps = int(deg/360) * STEPS;
-  myStepper.step(steps);
+  for (int i = 0; i < steps; i++) {
+    myStepper1.step(1);
+    myStepper2.step(-1);
+  }
 }
