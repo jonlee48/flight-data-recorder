4fa113fbc0d192526926ef8dbafded59ad5c0cc9
Renaud2002 <52769325+Renaud2002@users.noreply.github.com>
2022-03-15 15:27:47 -0400

    nj

--- Arduino/dbf2022/smc_tabletop/smc_tabletop.ino
+++ Arduino/dbf2022/smc_tabletop/smc_tabletop.ino
@@ -5,7 +5,7 @@
  *  See wiring diagram on TinkerCAD
  */
  
-#include <AccelStepper.h> // Make sure to import this libary 
+#include <Stepper.h> // Make sure to import this libary 
 
  
 /* Set the I/O pins here */
@@ -16,27 +16,28 @@
 #define BACK_PIN    6  // button to move belt back by INTERVAL
 #define BUILTIN_LED 13 // LED on Arduino, not set to do anything currently
 
+#define INPUT_SIGNAL A0
+
 /* Motor Specs */
 #define STEPS       2048  // drive mode
 #define MOTOR_PIN1  8     // IN1 on the ULN2003 driver
 #define MOTOR_PIN2  9     // IN2 on the ULN2003 driver
 #define MOTOR_PIN3  10    // IN3 on the ULN2003 driver
 #define MOTOR_PIN4  11    // IN4 on the ULN2003 driver
-#define ACCEL       100000.0  // set to practically instantaneous accel
-#define MAX_SPEED   15.0  // just go fast
+#define MAX_SPEED   10.0  // just go fast
 
 /* SMC Parameters (all distances in inches) */
-#define NUM_BOXES     6    // num boxes that start on belt
-#define SERVO_RADIUS  1.0  // radius of the drive wheel
-#define BELT_LENGTH   24.0 // distance btwn center of drive wheels
-#define BOX_WIDTH     3.0  // width of box on belt
-#define DIST_BTWN_BOX 1.0  // distance between boxes on belt
-#define XTRA_PUSH     0.0  // extra distance to push box from end of belt
-#define INTERVAL      1.0  // when forward/back buttons are pressed, move belt by this amount
+#define NUM_BOXES     2    // num boxes that start on belt
+#define SERVO_RADIUS  0.375  // radius of the drive wheel
+#define BELT_LENGTH   13.75 // distance btwn center of drive wheels
+#define BOX_WIDTH     2.5  // width of box on belt
+#define DIST_BTWN_BOX 0.0625  // distance between boxes on belt
+#define XTRA_PUSH     0.4475  // extra distance to push box from end of belt
+#define INTERVAL      0  // when forward/back buttons are pressed, move belt by this amount
 //#define THRESHOLD 1500   // PWM value to trigger deployment when connected to receiver
 
 // have to initialize an AccelStepper otherwise i get a segmentation fault
-AccelStepper stepper1(STEPS, MOTOR_PIN1, MOTOR_PIN3, MOTOR_PIN2, MOTOR_PIN4);
+Stepper stepper1(STEPS, MOTOR_PIN1, MOTOR_PIN3, MOTOR_PIN2, MOTOR_PIN4);
 
 /* Global Vars */
 int n = NUM_BOXES;
@@ -64,30 +65,40 @@ double dist_to_degrees(double dist) {
 void setup(void)
 {
   // onboard LED
-  pinMode(BUILTIN_LED, OUTPUT);
+  //pinMode(BUILTIN_LED, OUTPUT);
 
   // buttons
   pinMode(DEPLOY_PIN, INPUT_PULLUP);
-  pinMode(RESET_PIN, INPUT_PULLUP);
-  pinMode(FORWARD_PIN, INPUT_PULLUP);
-  pinMode(BACK_PIN, INPUT_PULLUP);
-
+  //pinMode(RESET_PIN, INPUT_PULLUP);
+  //pinMode(FORWARD_PIN, INPUT_PULLUP);
+  //pinMode(BACK_PIN, INPUT_PULLUP);
+  pinMode(INPUT_SIGNAL, INPUT);
   // Motor
-  stepper1.setMaxSpeed(MAX_SPEED);
+  //stepper1.setMaxSpeed(MAX_SPEED);
   stepper1.setSpeed(MAX_SPEED);
-  stepper1.setAcceleration(ACCEL);  
+  //stepper1.setAcceleration(ACCEL);  
 
   // For debugging
   Serial.begin(9600);
 }
 
+void moveDegrees(int deg) {
+  // convert degrees to steps
+  int steps = int(deg/360) * STEPS;
+  stepper1.step(steps);
+  
+  //for (int i = 0; i < steps; i++) {
+    //myStepper1.step(1);
+    //myStepper2.step(-1);
+  //}
+}
 
 void loop(void) {  
   // Uncomment these lines if using reciever to control belt
   //int pwm_value = pulseIn(DEPLOY_PIN, HIGH);
   //if (pwm_value > threshold && pwm_value < 2000){
-
-  if (digitalRead(DEPLOY_PIN) == LOW && n > 0) {
+  int pwm_value = pulseIn(INPUT_SIGNAL, HIGH);
+  if ((digitalRead(DEPLOY_PIN) == LOW && n > 0)||((pwm_value < 1200)&&(pwm_value > 1000))) {
     Serial.print("deploying box: ");
     Serial.print(n);
     // always assumes current position is centered
@@ -100,8 +111,8 @@ void loop(void) {
     double dist_to_end = (BELT_LENGTH/2.0) - (dist_occupied_n/2.0);
     double deg_to_deploy = dist_to_degrees(dist_to_end + XTRA_PUSH);
     // turn motor by deg_to_deploy
-    stepper1.move(deg_to_deploy);
-    stepper1.runToPosition();
+    moveDegrees(deg_to_deploy);
+    
 
     Serial.println("deploying");
     
@@ -113,31 +124,32 @@ void loop(void) {
     double diff_occupied = (dist_occupied_n - dist_occupied(n))/2.0;
     double deg_to_center = dist_to_degrees(diff_occupied) - deg_to_deploy;
     // turn motor by deg_to_center
-    stepper1.move(deg_to_center);
-    stepper1.runToPosition();
+    moveDegrees(deg_to_center);
+    //stepper1.runToPosition();
 
     Serial.println("returning to position");
     
   }
-
-  if (digitalRead(FORWARD_PIN) == LOW) {
-    double deg = dist_to_degrees(INTERVAL);
-    // turn motor by deg
-    stepper1.move(deg);
-    stepper1.runToPosition();
-    
-  }
-
-  if (digitalRead(BACK_PIN) == LOW) {
-    double deg = -1.0 * dist_to_degrees(INTERVAL);
-    // turn motor by deg
-    stepper1.move(deg);
-    stepper1.runToPosition();
-    
-  }
-
-  if (digitalRead(RESET_PIN) == LOW) {
-    n = NUM_BOXES; 
-  }
+  
+  
+//  if (digitalRead(FORWARD_PIN) == LOW) {
+//    double deg = dist_to_degrees(INTERVAL);
+//    // turn motor by deg
+//    moveDegree(deg);
+//    stepper1.runToPosition();
+//    
+//  }
+//
+//  if (digitalRead(BACK_PIN) == LOW) {
+//    double deg = -1.0 * dist_to_degrees(INTERVAL);
+//    // turn motor by deg
+//    moveDegrees(deg);
+//    stepper1.runToPosition();
+//    
+//  }
+//
+//  if (digitalRead(RESET_PIN) == LOW) {
+//    n = NUM_BOXES; 
+//  }
 
 }
--- Arduino/dbf2022/tests/stepper_buttons/stepper_buttons.ino
+++ Arduino/dbf2022/tests/stepper_buttons/stepper_buttons.ino
@@ -5,15 +5,17 @@
 
 #define STEPS 2048     // change this to fit the number of steps per revolution
 
+#define INPUT_SIGNAL A0
+
 // initialize the stepper library on pins 8 through 11:
 Stepper myStepper1(STEPS, 8, 10, 9, 11);
-Stepper myStepper2(STEPS, 7, 4, 6, 2);
+//Stepper myStepper2(STEPS, 7, 4, 6, 2);
 
 
 void setup() {
   // set the speed at 10 rpm:
   myStepper1.setSpeed(10);
-  myStepper2.setSpeed(10);
+  //myStepper2.setSpeed(10);
   // initialize the serial port:
   Serial.begin(9600);
 
@@ -21,25 +23,36 @@ void setup() {
   // buttons
   pinMode(BACK_PIN, INPUT_PULLUP);
   pinMode(FORWARD_PIN, INPUT_PULLUP);
+
+  // 
+  pinMode(INPUT_SIGNAL, INPUT);
   
 }
 
+
+//const int threshold = 1200;
+
 void loop() {
-  if (digitalRead(FORWARD_PIN) == LOW) {
+  int pwm_value = pulseIn(INPUT_SIGNAL, HIGH);
+  Serial.println(pwm_value);
+  if ((pwm_value < 1200)&&(pwm_value > 1000)) {
     moveDegrees(360);
   }
 
-  if (digitalRead(BACK_PIN) == LOW) {
-    moveDegrees(-360);
-  }
+  //if (digitalRead(BACK_PIN) == LOW) {
+    //moveDegrees(-360);
+    //Serial.println("pushed");
+  //}
 }
 
 // positive is clockwise
 void moveDegrees(int deg) {
   // convert degrees to steps
   int steps = int(deg/360) * STEPS;
-  for (int i = 0; i < steps; i++) {
-    myStepper1.step(1);
-    myStepper2.step(-1);
-  }
+  myStepper1.step(steps);
+  
+  //for (int i = 0; i < steps; i++) {
+    //myStepper1.step(1);
+    //myStepper2.step(-1);
+  //}
 }
